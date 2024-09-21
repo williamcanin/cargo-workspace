c2f959165415c1abf84cb140090df3964144960d
William C. Canin <william.costa.canin@gmail.com>
2024-09-20 23:00:43 -0300

    Adding help menu

--- src/menu.rs
+++ src/menu.rs
@@ -1,11 +1,27 @@
+use std::process::exit;
+
 use crate::utils::binary_name;
 use colored::*;
 
-pub struct Menu<'a> {
-  pub args: &'a Vec<String>,
+pub struct Menu {
+  pub args: Vec<String>,
 }
 
-impl Menu<'_> {
+impl Menu {
+  pub fn help(&self) {
+    println!("Usage: {} <command> <path> ...", binary_name());
+    println!("\n{}", "Commands:".bright_green());
+    println!(
+      "  {}                   Create new workspace.",
+      "new".bright_cyan()
+    );
+    println!(
+      "  {}             Print this message and exit.\n",
+      "-h,--help".bright_cyan()
+    );
+    println!("(C) Copyright 2024 - Cargo Workspace - All right reserved.");
+    exit(0);
+  }
   pub fn new(&self) -> Option<String> {
     let mut opt = None;
     let mut i = 1;
@@ -17,17 +33,16 @@ impl Menu<'_> {
             i += 2;
           } else {
             println!("{} 'new' requires a <PATH> argument", " Error: ".on_red());
-            println!("Usage: {} new <PATH>", binary_name());
             return None;
           }
         }
+        "--help" | "-h" | "help" => self.help(),
         _ => {
           println!(
             "{} Unknown option: {}",
             " Warning: ".on_yellow().black(),
             self.args[i]
           );
-          println!("Usage: {} new <PATH>", binary_name());
           return None;
         }
       }
