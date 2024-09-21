b904ac246666e1163b845ff9a059ae7d76845ec0
William C. Canin <william.costa.canin@gmail.com>
2024-09-20 22:58:40 -0300

    Removing panic message for Err

--- src/fs.rs
+++ src/fs.rs
@@ -1,6 +1,5 @@
-use colored::Colorize;
+use colored::*;
 use std::{error::Error, fs::File, io::Write, path::PathBuf};
-// use crate::path::Directory;
 
 pub struct FileCreate {
   pub content: String,
@@ -8,23 +7,25 @@ pub struct FileCreate {
 
 impl FileCreate {
   pub fn new(&self, path: &PathBuf, filename: &str) -> Result<bool, Box<dyn Error>> {
-    // if !Directory::create(path)? {
-    //   return Ok(false);
-    // }
-
     match File::create(path.join(filename)) {
       Ok(mut file) => match file.write_all(self.content.as_bytes()) {
         Ok(_) => Ok(true),
-        Err(_) => panic!(
-          "{} Failed to write to {} file",
-          " Error: ".on_red(),
-          path.join(filename).to_str().unwrap()
+        Err(_) => Err(
+          format!(
+            "{} Failed to write to {} file",
+            " Error: ".on_red(),
+            path.join(filename).to_str().unwrap()
+          )
+          .into(),
         ),
       },
-      Err(_) => panic!(
-        "{} Failed to create {}",
-        " Error: ".on_red(),
-        path.join(filename).to_str().unwrap()
+      Err(_) => Err(
+        format!(
+          "{} Failed to create {}",
+          " Error: ".on_red(),
+          path.join(filename).to_str().unwrap()
+        )
+        .into(),
       ),
     }
   }
