86422038be200fccba2c85a44e42c42f1e1f771f
William C. Canin <william.costa.canin@gmail.com>
2024-09-20 23:00:17 -0300

    Using if let to trigger errors, adding help, and more

--- src/main.rs
+++ src/main.rs
@@ -12,63 +12,60 @@ use fs::FileCreate;
 use git::Git;
 use path::Directory;
 use std::{env, error::Error, path::PathBuf, process::exit};
-use utils::binary_name;
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let args: Vec<String> = env::args().collect();
-  let menu = Menu { args: &args }.new();
+  let menu = Menu {
+    args: env::args().collect(),
+  };
 
-  if menu.is_none() {
-    exit(1);
-  }
-
-  match menu {
+  match menu.new() {
     Some(folder) => {
       let folder = folder.trim();
       let root = PathBuf::from(folder);
 
-      // ------- Ignore if workspace already exists -------
-      if root.join("Cargo.toml").exists() {
-        println!(
-          "{} The workspace `{folder}` already exists",
-          " Error: ".on_red()
-        );
+      // ------- Create root workspace -------
+      if let Err(e) = Directory::create(&root) {
+        println!("{}", e);
         exit(1);
       }
 
-      // ------- Create root workspace -------
-      Directory::create(&root)?;
-
       // ------- Creates the workspace Cargo.toml -------
-      FileCreate {
+      let instance = FileCreate {
         content: String::from(constants::CARGO_TOML_CONTENT),
+      };
+      if let Err(e) = instance.new(&root, "Cargo.toml") {
+        println!("{}", e);
+        exit(1);
       }
-      .new(&root, "Cargo.toml")?;
 
       // ------- Create .cargo folder -------
-      Directory::create(&root.join(".cargo"))?;
+      if let Err(e) = Directory::create(&root.join(".cargo")) {
+        println!("{}", e);
+        exit(1);
+      }
 
       // ------- Creates the `cargo` configuration file -------
-      FileCreate {
+      let instance = FileCreate {
         content: String::from(constants::CARGO_CONFIG_CONTENT),
+      };
+      if let Err(e) = instance.new(&root.join(".cargo"), "config.toml") {
+        println!("{}", e);
+        exit(1);
       }
-      .new(&root.join(".cargo"), "config.toml")?;
 
       // ------- Start `git init` in the workspace -------
-      Git {
+      let instance = Git {
         content: String::from(constants::GIT_IGNORE_CONTENT),
-      }
-      .init(&root)?;
+      };
+      if let Err(e) = instance.init(&root) {
+        println!("{}", e);
+        exit(1);
+      };
 
       println!("    {} `{folder}` workspace skeleton", "Creating".green());
     }
     None => {
-      println!(
-        "{} Usage: {} new <PATH>",
-        " Warning: ".on_yellow().black(),
-        binary_name()
-      );
-      exit(1);
+      menu.help();
     }
   }
 
